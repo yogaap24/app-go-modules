607a2762ce76a2a4a9fbaaa3581f8990c058e2ef
Yoga Agung Prasetio <yoga.agung@transtrack.id>
2022-09-26 11:00:11 +0700

    Implemantation golang modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yogaap24/app-go-modules
+
+go 1.17
+
+require github.com/yogaap24/belajar-go-modules v1.0.1
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	belajar_go_modules "github.com/yogaap24/belajar-go-modules"
+)
+
+func main() {
+	fmt.Println(belajar_go_modules.SayHello("Yoga"))
+}
